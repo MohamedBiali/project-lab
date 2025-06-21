7469199247d18b90ab1b716853e9f93a03498725
MohamedBiali <mohamedkhaledbiali@gmail.com>
2025-06-21 21:38:16 +0300

    Some commit message of your choice goes here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Nav from './component/Nav.js';
+import Main from './component/Main.js';
+import Footer from './component/Footer.js';
+import Head from './component/Head.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-<div>Little Lemon</div>
-    </div>
+   <div className='App'>
+
+   
+<Head />
+
+<Nav/>
+<Main/>
+<Footer/>
+  </div>
   );
 }
 
---
+++ src/component/Head.js
@@ -0,0 +1,12 @@
+
+
+
+function Head() {
+  return (
+   <div>
+<h2>Header</h2>
+   <img src='../images/41_2023-638314326832820025-282.jpg' alt='Description'/>
+   </div>
+  )
+}
+export default Head;
